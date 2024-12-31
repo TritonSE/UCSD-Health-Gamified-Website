fab97cc6544c84b0f3e85fd17ac44490a30fa132
Eshaan Sharma <eshaansharma18@gmail.com>
2024-12-31 13:28:15 -0700

    backend ESLint and Prettier

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+/* eslint-disable */
+
+import express, { Request, Response } from "express";
+
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => {
-    res.send('Hello from the Express backend!');
+app.get("/", (req, res) => {
+  res.send("Hello from the Express backend!");
 });
 
 app.listen(port, () => {
-    console.log(`Backend running at http://localhost:${port}`);
+  console.log(`Backend running at http://localhost:${port}`);
 });
