157457bcf9daae6f3b484b0dc39ef72aea3e6e19
shashwat <shashwat.bhandari.14@gmail.com>
2025-01-23 16:11:08 -0800

    remove files and fix frontend lint

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
-import leftArrow from "../../public/left_arrow.svg";
-
 import LeftSide from "./components/leftSide";
-import styles from "./page.module.css";
 import { LoginButton } from "./components/loginButton";
 import { TextBox } from "./components/textBox";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -18,7 +14,21 @@ export default function Home() {
       {/* right side */}
       <div className={styles.rightSide}>
         <span className={styles.back}>
-          <Image width="100" height="100" src={leftArrow} alt="test" />
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M14.7071 6.29289C15.0976 6.68342 15.0976 7.31658 14.7071 7.70711L10.4142 12L14.7071 16.2929C15.0976 16.6834 15.0976 17.3166 14.7071 17.7071C14.3166 18.0976 13.6834 18.0976 13.2929 17.7071L8.29289 12.7071C7.90237 12.3166 7.90237 11.6834 8.29289 11.2929L13.2929 6.29289C13.6834 5.90237 14.3166 5.90237 14.7071 6.29289Z"
+              fill="#1C3A29"
+            />
+          </svg>
+
           <p>Back to Sign-in</p>
         </span>
 
