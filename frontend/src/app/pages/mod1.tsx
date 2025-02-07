aed248563c4f2f4beb460d370b559b1d00fd263d
shashwat <shashwat.bhandari.14@gmail.com>
2025-02-06 18:55:21 -0800

    add card slides component and fix frontend lint

--- frontend/src/app/pages/mod1.tsx
+++ frontend/src/app/pages/mod1.tsx
@@ -1,6 +1,5 @@
-
-import ModTitle from "../components/ModTitle";
 import Mod1Intro from "../components/Mod1Intro";
+import ModTitle from "../components/ModTitle";
 
 import styles from "./mod1.module.css";
 
@@ -8,12 +7,10 @@ export default function Module1() {
   return (
     <div className={styles.container}>
       {/* sidebar (temporary one, this should be replaced later) */}
-      <div className={styles.sidebar}>
-        Sidebar!
-      </div>
+      <div className={styles.sidebar}>Sidebar!</div>
       {/* content */}
       <div className={styles.content}>
-        <ModTitle module_num={1} module_name="What is an E-bike?"/>
+        <ModTitle module_num={1} module_name="What is an E-bike?" />
         <Mod1Intro />
       </div>
     </div>
