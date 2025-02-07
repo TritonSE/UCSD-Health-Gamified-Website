37dd8edc60fb5710f2e32cddeb609f49adaf6448
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2025-02-06 19:11:01 -0800

    Create factor title component

--- frontend/src/app/pages/mod1.tsx
+++ frontend/src/app/pages/mod1.tsx
@@ -1,3 +1,4 @@
+import Mod1FactorTitle from "../components/Mod1FactorTitle";
 import Mod1Intro from "../components/Mod1Intro";
 import ModTitle from "../components/ModTitle";
 
@@ -12,6 +13,9 @@ export default function Module1() {
       <div className={styles.content}>
         <ModTitle module_num={1} module_name="What is an E-bike?" />
         <Mod1Intro />
+        <br />
+        <br />
+        <Mod1FactorTitle number={1} step="What types of riding do YOU want to do?" />
       </div>
     </div>
   );
