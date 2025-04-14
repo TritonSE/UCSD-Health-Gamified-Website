07457742819a1ac1ab95d0485b92a2c5a08cfbe9
YifeiXue111 <156253841+YifeiXue111@users.noreply.github.com>
2025-04-14 16:44:58 -0700

    still have problem when sidebar does not collapes

--- frontend/src/app/components/Mod2Components/SimpleWay/SimpleWay.tsx
+++ frontend/src/app/components/Mod2Components/SimpleWay/SimpleWay.tsx
@@ -1,18 +1,20 @@
-import Checkbox from "../../Checkbox/Checkbox";
+"use client";
 
+import Checkbox from "../../Checkbox/Checkbox";
 import styles from "./SimpleWay.module.css";
 
 export default function SimpleWay() {
   return (
-    <section id={styles.container}>
+    <section className={styles.container}>
       <div className={styles.header_container}>
         <h2 className={styles.headerText}>A SIMPLE WAY TO REMEMBER THE MAIN STUFF</h2>
-        <p className={styles.text}>
+        <p className={styles.headerSubtext}>
           By performing a regular ABC bike check, you can identify and address potential problems
           before they become more serious.
         </p>
       </div>
-      <Checkbox />
+        <Checkbox />
+        <p className={styles.text}>Click checklist to see the tips on the right!</p>
     </section>
   );
 }
