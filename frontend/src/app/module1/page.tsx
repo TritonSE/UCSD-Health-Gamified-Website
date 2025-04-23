632def2740d55b115d2c97ac33e6d06bd6784696
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2025-04-23 01:59:53 -0700

    Create let's talk section

--- frontend/src/app/module1/page.tsx
+++ frontend/src/app/module1/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Sidebar from "../components/HomePage/Sidebar/Sidebar";
-import MiniQuiz from "../components/module1/MiniQuiz";
 import Mod1ChooseBike from "../components/module1/Mod1ChooseBike";
 import Mod1Classes from "../components/module1/Mod1Classes";
 import Mod1Factor1 from "../components/module1/Mod1Factor1";
@@ -9,6 +8,7 @@ import Mod1Factor2 from "../components/module1/Mod1Factor2";
 import Mod1Factor3 from "../components/module1/Mod1Factor3";
 import Mod1Factor5 from "../components/module1/Mod1Factor5";
 import Mod1Intro from "../components/module1/Mod1Intro";
+import Mod1LetsTalk from "../components/module1/Mod1LetsTalk";
 import Mod1WhyRide from "../components/module1/Mod1WhyRide";
 import ModTitle from "../components/module1/ModTitle";
 
@@ -23,20 +23,28 @@ export default function Module1() {
       </div>
       {/* content */}
       <div className={styles.content}>
-        {/* title and intro */}
+        {/* frame 1 - intro */}
         <ModTitle module_num={1} module_name="WHAT IS AN E BIKE?" />
         <Mod1Intro />
+        {/* frame 2 - choose */}
         <Mod1ChooseBike />
-        {/* factors */}
+        {/* frame 3 - types of riding */}
         <Mod1Factor1 />
+        {/* frame 4 - consider physical */}
         <Mod1Factor2 />
+        {/* frame 5 - evaluate features*/}
         <Mod1Factor3 />
-        {/* NOTE: Factor4 was merged into 5 */}
+        {/* frame 6 - consider e bike classification */}
         <Mod1Factor5 />
+        {/* frame 7 - classes table */}
         <Mod1Classes />
+        {/* frame 8 - why ride */}
         <Mod1WhyRide />
-        {/* TODO: Modify component below + change name */}
-        <MiniQuiz />
+        {/* frame 9 - let's talk */}
+        <Mod1LetsTalk />
+        {/* frame 10 - risks with illegal */}
+
+        {/* frame 11 - quiz */}
       </div>
     </div>
   );
