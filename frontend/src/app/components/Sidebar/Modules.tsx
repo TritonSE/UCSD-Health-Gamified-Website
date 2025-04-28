e2e3fdb68686cabe1ce7e4fa6dd4d7fd525e1b8f
km322 <mittal.ketan1@gmail.com>
2025-04-28 00:48:22 -0700

    made modules dynamic

--- frontend/src/app/components/Sidebar/Modules.tsx
+++ frontend/src/app/components/Sidebar/Modules.tsx
@@ -1,7 +1,7 @@
 import { Module } from "./Module";
 import styles from "./Modules.module.css";
 
-export const Modules = ({ isCollapsed = false }) => {
+export const Modules = ({ isCollapsed = false, currentModule = 1 }) => {
   let buttonClass = styles.moduleContainer;
   if (isCollapsed) {
     buttonClass += ` ${styles.collapsed}`;
@@ -11,86 +11,47 @@ export const Modules = ({ isCollapsed = false }) => {
     console.log("Module clicked");
   };
 
+  const moduleData = [
+    { name: "What is an E-bike?", time: 10, number: 1 },
+    { name: "Maintaining Your E-Bike", time: 10, number: 2 },
+    { name: "Safety Equipment", time: 10, number: 3 },
+    { name: "5 Need to Know Principles", time: 10, number: 4 },
+    { name: "Rules of the Road", time: 10, number: 5 },
+    { name: "In Case of a Collision", time: 10, number: 6 },
+    { name: "Teen & Parent Awareness", time: 10, number: 7 },
+    { name: "Closing Video", time: 10, number: 8 },
+    { name: "Test", time: 10, number: 9, addLine: false },
+  ];
+
   return (
     <div>
       <div className={buttonClass}>
         <span className={styles.title}>Modules</span>
       </div>
       <div className={styles.modules}>
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="What is an E-bike?"
-          moduleTime={10}
-          moduleNumber={1}
-          handleClick={handleClick}
-          kind="complete"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="Maintaining Your E-Bike"
-          moduleTime={10}
-          moduleNumber={2}
-          handleClick={handleClick}
-          kind="complete"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="Safety Equipment"
-          moduleTime={10}
-          moduleNumber={3}
-          handleClick={handleClick}
-          kind="complete"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="5 Need to Know Principles"
-          moduleTime={10}
-          moduleNumber={4}
-          handleClick={handleClick}
-          kind="complete"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="Rules of the Road"
-          moduleTime={10}
-          moduleNumber={5}
-          handleClick={handleClick}
-          kind="primary"
-          highlighted={true}
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="In Case of a Collision"
-          moduleTime={10}
-          moduleNumber={6}
-          handleClick={handleClick}
-          kind="inactive"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="Teen & Parent Awareness"
-          moduleTime={10}
-          moduleNumber={7}
-          handleClick={handleClick}
-          kind="inactive"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="Closing Video"
-          moduleTime={10}
-          moduleNumber={8}
-          handleClick={handleClick}
-          kind="inactive"
-        />
-        <Module
-          isCollapsed={isCollapsed}
-          moduleName="Test"
-          moduleTime={10}
-          moduleNumber={9}
-          addLine={false}
-          handleClick={handleClick}
-          kind="inactive"
-        />
+        {moduleData.map((module) => {
+          // Determine module kind based on its number compared to currentModule
+          let kind: "primary" | "inactive" | "complete" = "inactive";
+          if (module.number < currentModule) {
+            kind = "complete";
+          } else if (module.number === currentModule) {
+            kind = "primary";
+          }
+
+          return (
+            <Module
+              key={module.number}
+              isCollapsed={isCollapsed}
+              moduleName={module.name}
+              moduleTime={module.time}
+              moduleNumber={module.number}
+              handleClick={handleClick}
+              kind={kind}
+              highlighted={module.number === currentModule}
+              addLine={module.addLine !== false}
+            />
+          );
+        })}
       </div>
     </div>
   );
