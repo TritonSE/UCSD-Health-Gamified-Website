09d0cd44e8158bbf462eadea1c49b14bda3dbfc1
shashwat <shashwat.bhandari.14@gmail.com>
2025-05-05 18:12:50 -0700

    complete recommendation

---
+++ frontend/src/app/components/module3/Recommendation.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+import styles from "./Recommendation.module.css";
+
+export default function Recommendation() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recommendation</h1>
+      <div className={styles.svgs}>
+        <Image
+          width="301"
+          height="231"
+          src="/module3/helmet_rated.svg"
+          alt="We recommend using helmets rated for higher speeds which will provide additional head coverage and protection!"
+          className={styles.icon}
+        />
+        <Image
+          width={382}
+          height={461}
+          src="/TimmyHelmet.svg"
+          alt="Timmy with helmet"
+          className={styles.icon}
+        />
+        <Image
+          width="240"
+          height="700"
+          src="/module3/moped_rated.svg"
+          alt="Moped rated or light motorcycle rated helmets will provide greater protection when compared to bicycle helmets."
+          className={styles.icon}
+        />
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/module3/page.tsx
+++ frontend/src/app/module3/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ModuleSliderContainer from "../components/ModuleSliderContainer/ModuleSliderContainer";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Helmet from "../components/module3/Helmet";
+import Recommendation from "../components/module3/Recommendation";
 import SafetyEquipment from "../components/module3/SafetyEquipment";
 
 import styles from "./mod3.module.css";
@@ -17,6 +18,8 @@ export default function Module1() {
       {/* content */}
       <ModuleSliderContainer moduleText="MODULE 3: Helmet Choice and Safety Equipment">
         <Helmet />
+        <Helmet />
+        <Recommendation />
         <SafetyEquipment />
       </ModuleSliderContainer>
     </div>
