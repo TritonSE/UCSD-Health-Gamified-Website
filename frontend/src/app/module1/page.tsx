8a40d705c99a4b0dd6246c105b05d02e9bc36d7d
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2025-05-08 16:01:41 -0700

    Add ModuleIntro

--- frontend/src/app/module1/page.tsx
+++ frontend/src/app/module1/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
+import ModuleIntro from "../components/AllModules/ModuleIntro/ModuleIntro";
 import Sidebar from "../components/HomePage/Sidebar/Sidebar";
 import ModuleSliderContainer from "../components/ModuleSliderContainer/ModuleSliderContainer";
 import Mod1ChooseBike from "../components/module1/Mod1ChooseBike";
@@ -10,7 +12,6 @@ import Mod1Factor1 from "../components/module1/Mod1Factor1";
 import Mod1Factor2 from "../components/module1/Mod1Factor2";
 import Mod1Factor3 from "../components/module1/Mod1Factor3";
 import Mod1Factor5 from "../components/module1/Mod1Factor5";
-import Mod1Intro from "../components/module1/Mod1Intro";
 import Mod1LetsTalk from "../components/module1/Mod1LetsTalk";
 import Mod1Risks from "../components/module1/Mod1Risks";
 import Mod1WhyRide from "../components/module1/Mod1WhyRide";
@@ -26,7 +27,27 @@ export default function Module1() {
       {/* content */}
       <ModuleSliderContainer moduleText="MODULE 1: WHAT IS AN E BIKE?">
         {/* frame 1 - intro */}
-        <Mod1Intro />
+        <ModuleIntro
+          moduleNumber={1}
+          title="WHAT IS AN E BIKE?"
+          subtitle="INTRODUCTION"
+          description={
+            <p>
+              E Bikes are popular for commuting, recreation, and transportation. To ensure a safe
+              and enjoyable experience, it&#39;s crucial to understand the different E Bike
+              classifications and the importance of riding legal E Bikes. This module will guide you
+              through the process of choosing the best E Bike for your needs.{" "}
+            </p>
+          }
+          Mascot={
+            <Image
+              src="/TimmyRiding.svg"
+              width={252}
+              height={329}
+              alt="Timmy the tire riding a bike"
+            />
+          }
+        />
         {/* frame 2 - choose */}
         <Mod1ChooseBike />
         {/* frame 3 - types of riding */}
