8d409da0a042d8c02046d6cad91347f569e6b3ac
Eshaan Sharma <eshaansharma18@gmail.com>
2025-05-11 15:52:06 -0600

    fixed sidebar homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
+      <div className={styles.sidebarWrapper}>
         <Sidebar />
       </div>
       <ModuleMap />
