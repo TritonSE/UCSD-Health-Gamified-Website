908f84d3881e9daa256ebdfaa3a3f386c3178ce6
km322 <mittal.ketan1@gmail.com>
2025-05-13 20:21:58 -0700

    first page

---
+++ frontend/src/app/components/module7/BikeInjuries.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react";
+
+import styles from "./BikeInjuries.module.css";
+
+const Page2: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>ARE YOU WEARING YOUR HELMET CORRECTLY?</h1>
+
+      <div className={styles.content}>
+        <div className={styles.textSection}>
+          <h3 className={styles.subtitle}>Incorrect Ways</h3>
+          <div className={`${styles.point} ${styles.point1}`}>
+            <span className={styles.number}>1.</span>
+            <p>
+              &nbsp;<strong>Too Far Back:</strong> Leaves your forehead exposed, reducing
+              &nbsp;protection.
+            </p>
+          </div>
+          <div className={`${styles.point} ${styles.point2}`}>
+            <span className={styles.number}>2.</span>
+            <p>
+              <strong>Too Far Forward:</strong> Obstructs vision and feels uncomfortable.
+            </p>
+          </div>
+          <div className={`${styles.point} ${styles.point3}`}>
+            <span className={styles.number}>3.</span>
+            <p>
+              <strong>Loose Straps:</strong> Won’t keep the helmet secure in a crash.
+            </p>
+          </div>
+          <div className={`${styles.point} ${styles.point4}`}>
+            <span className={styles.number}>4.</span>
+            <p>
+              <strong>Unbuckled Chin Strap:</strong> Helmet can fall off during impact, making it
+              useless.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.imageSection}>
+          <Image
+            src="/module3/wrong_helmet_pos.svg"
+            width="250"
+            height="250"
+            alt="Incorrect Helmet Positions"
+            className={styles.image}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page2;
---
+++ frontend/src/app/module7/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+import ModuleIntro from "../components/AllModules/ModuleIntro/ModuleIntro";
+import ModuleSliderContainer from "../components/ModuleSliderContainer/ModuleSliderContainer";
+import Sidebar from "../components/Sidebar/Sidebar";
+import BikeInjuries from "../components/module7/BikeInjuries";
+
+import styles from "./mod7.module.css";
+
+export default function Module7() {
+  return (
+    <div className={styles.container}>
+      {/* sidebar */}
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
+      {/* content */}
+      <ModuleSliderContainer moduleText="MODULE 7: Teen & Parent education & Awareness">
+        <ModuleIntro
+          moduleNumber={7}
+          title="Teen & Parent education & Awareness"
+          subtitle="Guiding Safe Riding Together"
+          description={
+            <p>
+              <strong>Please complete this section with a parent or guardian.</strong> As E Bikes
+              become an increasingly popular mode of transportation for teens, it is essential for
+              families to understand the associated risks and best safety practices. This section
+              equips parents and guardians with the knowledge and tools needed to actively support
+              safe riding habits, minimize risks, and foster responsible decision-making in young
+              riders.
+            </p>
+          }
+          Mascot={
+            <Image
+              src="/module7/TimmyParents.svg"
+              alt="Timmy the tire"
+              width={320}
+              height={329}
+              className={styles.mascot}
+            />
+          }
+        />
+        <BikeInjuries />
+      </ModuleSliderContainer>
+    </div>
+  );
+}
