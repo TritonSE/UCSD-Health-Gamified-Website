ddc9e751c06677315ae5ad5c11e54fef12ef63e8
Sweekrit-B <sweekrit.bhatnagar@gmail.com>
2025-05-21 18:34:25 -0700

    Stashing again

--- frontend/src/app/components/module1/Mod1ChooseBike.tsx
+++ frontend/src/app/components/module1/Mod1ChooseBike.tsx
@@ -4,22 +4,20 @@ import styles from "./Mod1ChooseBike.module.css";
 
 export default function Mod1ChooseBike() {
   return (
-    <div className={styles.container}>
+    <section id={styles.container}>
       <h1 className={styles.title}>CHOOSING THE RIGHT E BIKE</h1>
       <p className={styles.description}>
         Why choose an E Bike? E Bikes are a fun and eco-friendly way to get around. They can help
         you:
       </p>
-      <div className={styles.cardContainerWrapper}>
-        <div className={styles.cardContainer}>
-          <CardSlide title="SAVE TIME" content="Get to school faster without breaking a sweat." />
-          <CardSlide title="SAVE MONEY" content="Reduce transportation costs." />
-          <CardSlide
-            title="STAY ACTIVE"
-            content="Enjoy the benefits of cycling while getting a boost from the electric motor."
-          />
-        </div>
+      <div id={styles.cardContainer}>
+        <CardSlide title="SAVE TIME" content="Get to school faster without breaking a sweat." />
+        <CardSlide title="SAVE MONEY" content="Reduce transportation costs." />
+        <CardSlide
+          title="STAY ACTIVE"
+          content="Enjoy the benefits of cycling while getting a boost from the electric motor."
+        />
       </div>
-    </div>
+    </section>
   );
 }
