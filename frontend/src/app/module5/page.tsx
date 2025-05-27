9398446feb858462aad1f698630cf1c14f3d77a6
shashwat <shashwat.bhandari.14@gmail.com>
2025-05-26 21:25:53 -0700

    add section 2

--- frontend/src/app/module5/page.tsx
+++ frontend/src/app/module5/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Section1 from "../components/Mod2Components/Section1/Section1";
+import Section1 from "../components/Module5/Section1";
+import Section2 from "../components/Module5/Section2";
 import Section6 from "../components/Module5/Section6";
 import Section7 from "../components/Module5/Section7";
 import Section8 from "../components/Module5/Section8";
@@ -17,6 +18,7 @@ export default function Module5() {
       <ModuleSliderContainer moduleText="MODULE 5: Rules of the Road">
         {/* Temporary slides (Section1 borrowed from Module 2) - remove when actual page created*/}
         <Section1 />
+        <Section2 />
         <Section6 />
         <Section7 />
         <Section8 />
