8dd34859d314beb1c88cf1cd59838f61dceb9e33
Eshaan Sharma <eshaansharma18@gmail.com>
2025-05-27 17:43:14 -0700

    fixed closing video

--- frontend/src/app/components/Module8Components/ClosingVideo/ClosingVideo.tsx
+++ frontend/src/app/components/Module8Components/ClosingVideo/ClosingVideo.tsx
@@ -4,20 +4,18 @@ import styles from "./ClosingVideo.module.css";
 
 export default function ClosingVideo() {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.titleContainer}>
-          <div className={styles.title}>A Message From the City of Encinitas</div>
-        </div>
-        <div className={styles.ytContainer}>
-          <iframe
-            id="ytplayer"
-            width="854"
-            height="480"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          ></iframe>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.header_container}>
+        <h2>A Message From the City of Encinitas</h2>
       </div>
-    </main>
+      <div className={styles.ytContainer}>
+        <iframe
+          id="ytplayer"
+          width="854"
+          height="480"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        ></iframe>
+      </div>
+    </div>
   );
 }
