cfaaeaa0f937ef7ef2f0cb31a72cec610eef10ed
Eshaan Sharma <eshaansharma18@gmail.com>
2025-05-29 09:38:01 -0700

    fixed page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
-import styles from "./page.module.css";
-import Sidebar from "./components/Sidebar/Sidebar";
 import ModuleMap from "./components/HomePage/ModuleMap/ModuleMap";
+import Sidebar from "./components/Sidebar/Sidebar";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
