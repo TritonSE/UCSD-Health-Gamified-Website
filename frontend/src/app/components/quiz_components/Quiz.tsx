2ff776f64322b85f0fbcda93961adf11253b20fa
km322 <mittal.ketan1@gmail.com>
2025-06-29 18:27:10 -0700

    add open cert label

--- frontend/src/app/components/quiz_components/Quiz.tsx
+++ frontend/src/app/components/quiz_components/Quiz.tsx
@@ -140,7 +140,11 @@ export const Quiz = ({
       setSubmitted(false);
       setScore(0);
       setTitle(title);
-      setLabel("Next Module");
+      if (module === 9) {
+        setLabel("Open Certificate");
+      } else {
+        setLabel("Next Module");
+      }
 
       if (randomized) {
         setRandomizedQuestions(
@@ -257,7 +261,11 @@ export const Quiz = ({
     if (calculatedScore < 75) {
       setLabel("Retake Quiz");
     } else {
-      setLabel("Next Module");
+      if (module === 9) {
+        setLabel("Open Certificate");
+      } else {
+        setLabel("Next Module");
+      }
     }
 
     if (calculatedScore < 75 && calculatedScore > 74) {
