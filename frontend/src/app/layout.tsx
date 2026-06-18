24fbf66e2f0a45ea48ec89e1dd3981caf1ff6961
Eshaan Sharma <eshaansharma18@gmail.com>
2026-06-17 20:18:07 -0700

    fixed layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 import localFont from "next/font/local";
 
 import { AuthProvider } from "./contexts/AuthContext";
